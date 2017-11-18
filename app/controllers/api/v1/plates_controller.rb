9f3d15055def59937abd50f8d3a3e44fc0772b03
Marcio <marcio@mail.com>
2017-11-18 11:15:22 -0200

    Generating resources controllers

---
+++ app/controllers/api/v1/plates_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::PlatesController < Api::V1::ApiController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::RestaurantsController < Api::V1::ApiController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
