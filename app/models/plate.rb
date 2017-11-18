50595154c76b8c741f577032bc2e39f51a8b7096
Marcio <marcio@mail.com>
2017-11-18 15:15:06 -0200

    adjustment models

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,3 +1,4 @@
 class Plate < ApplicationRecord
   belongs_to :restaurant
+  validates :restaurant, presence: true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: :slugged  
+  has_many :plates
+  validates :name, presence: true
 end
